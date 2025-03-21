5a39b718bad395036fa12888299195aa0ac32f7e
annasybingco <sybingcoanna@gmail.com>
2025-03-21 19:11:48 -0400

    add mobile and desktop version of resume

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,10 +1,16 @@
-import resumeImg from "../../assets/images/resume.png";
 import "./Resume.scss";
+import resume from "../../assets/images/desktop-resume.png";
+import mobileResume from "../../assets/images/mobile-resume.png";
 
 function Resume() {
   return (
     <div className="resume">
-      <img src={resumeImg} alt="resume" className="resume__img" />
+      <img src={resume} alt="resume" className="resume__img" />
+      <img
+        src={mobileResume}
+        alt="mobile-resume"
+        className="resume__img-mobile"
+      />
       <button>Download PDF</button>
     </div>
   );
